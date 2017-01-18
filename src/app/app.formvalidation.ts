6901dfb77e930d14de9f5f62a4557b03ae9ce807
Harjot Singh <hsingh@sjcoe.net>
2017-01-18 15:25:03 -0800

    my changes

--- src/app/app.formvalidation.ts
+++ src/app/app.formvalidation.ts
@@ -3,55 +3,7 @@ import { FormGroup, FormBuilder,Validators  } from '@angular/forms';
 
 @Component({
   selector: 'form-validation',
-  template : `
-  <div class="jumbotron">
-    <h2>Form w/ Validations</h2>
-    <form [formGroup]="complexForm" (ngSubmit)="submitForm(complexForm.value)">
-      <div class="form-group" [ngClass]="{'has-error':!complexForm.controls['firstName'].valid}">
-        <label>First Name:</label>
-        <input class="form-control" type="text" placeholder="John" [formControl]="complexForm.controls['firstName']">
-      </div>
-      <!-- We are using the ngClass directive to conditionally add a class to our div if the form is invalid -->
-      <div class="form-group" [ngClass]="{'has-error':!complexForm.controls['lastName'].valid}">
-        <label>Last Name</label>
-        <input class="form-control" type="text" placeholder="Doe" [formControl]="complexForm.controls['lastName']">
-      </div>
-      <div class="form-group" [ngClass]="{'has-error':!complexForm.controls['gender'].valid}">
-        <label>Gender</label>
-        <!-- Here we are using the ngIf directive to display an error message if the user has not selected a gender since we can’t color the radio buttons. -->
-        <div class="alert alert-danger" *ngIf="!complexForm.controls['gender'].valid">You must select a gender.</div>
-      </div>
-      <div class="radio">
-        <label>
-          <input type="radio" name="gender" value="Male" [formControl]="complexForm.controls['gender']">
-          Male
-        </label>
-      </div>
-      <div class="radio">
-        <label>
-          <input type="radio" name="gender" value="Female" [formControl]="complexForm.controls['gender']">
-          Female
-        </label>
-      </div>
-      <div class="form-group">
-        <label>Activities</label>
-      </div>
-      <label class="checkbox-inline">
-        <input type="checkbox" value="hiking" name="hiking" [formControl]="complexForm.controls['hiking']"> Hiking
-      </label>
-      <label class="checkbox-inline">
-        <input type="checkbox" value="swimming" name="swimming" [formControl]="complexForm.controls['swimming']"> Swimming
-      </label>
-      <label class="checkbox-inline">
-        <input type="checkbox" value="running" name="running" [formControl]="complexForm.controls['running']"> Running
-      </label>
-      <div class="form-group">
-        <button type="submit" class="btn btn-primary" [disabled]="!complexForm.valid">Submit</button>
-
-      </div>
-    </form>
-  </div>
-  `
+  templateUrl : './app.Formvalidation.html'
 })
 export class FormValidationComponent {
   complexForm : FormGroup;
