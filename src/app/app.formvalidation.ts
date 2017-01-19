077e0f660191a583d16a278b49867da1c9065bb0
Harjot Singh <hsingh@sjcoe.net>
2017-01-18 16:21:15 -0800

    final push

--- src/app/app.formvalidation.ts
+++ src/app/app.formvalidation.ts
@@ -13,7 +13,7 @@ export class FormValidationComponent {
       // To add a validator, we must first convert the string value into an array. The first item in the array is the default value if any, then the next item in the array is the validator. Here we are adding a required validator meaning that the firstName attribute must have a value in it.
       'firstName' : [null, Validators.required],
       // We can use more than one validator per field. If we want to use more than one validator we have to wrap our array of validators with a Validators.compose function. Here we are using a required, minimum length and maximum length validator.
-      'lastName': [null, Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(10)])],
+      'lastName': [null, Validators.compose([Validators.required, Validators.minLength(20), Validators.maxLength(24)])],
       'gender' : [null, Validators.required],
       'hiking' : false,
       'running' :false,
